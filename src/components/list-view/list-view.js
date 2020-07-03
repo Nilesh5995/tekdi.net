61071be74ed1c76013d570f8c2a8565360a0435f
nilesh5995 <nilesh_s@techjoomla.com>
2020-07-03 13:36:07 +0530

    anchor links

--- src/components/list-view/list-view.js
+++ src/components/list-view/list-view.js
@@ -3,8 +3,17 @@ import { Link } from 'gatsby';
 import PreviewCompatibleImage from '../common/preview-compatible-image'
 import './list-view.scss';
 const renderList = ({ node }) => {
+  let str = node.frontmatter.title;
+  str = str.replace(/^\s+|\s+$/g, ""); // trim
+  str = str.toLowerCase();
+  str = str
+    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+    .replace(/\s+/g, "-") // collapse whitespace and replace by -
+    .replace(/-+/g, "-") // collapse dashes
+    .replace(/^-+/, "") // trim - from start of text
+    .replace(/-+$/, ""); // trim - from end of text
   return (
-     <div className="blog-list mb-5 pb-5" key={node.id}>
+     <div className="blog-list mb-5 pb-5" key={node.id} id={str}>>
        <div className="row">
         <div className="col-md-3 col-sm-4 col-xs-12">
           {node.frontmatter.image ? (
@@ -20,7 +29,7 @@ const renderList = ({ node }) => {
           </div>
           <div className="col-md-9 col-sm-8 col-xs-12 blog-info">
             <h3 className="blog-title">
-                {node.frontmatter.title}
+                {node.frontmatter.title}{str}
             </h3>
             <ul className="unstyled mb-4">
               {node.frontmatter.subTitle && node.frontmatter.subTitle !=null ? 
