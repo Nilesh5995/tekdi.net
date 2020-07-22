6c34ef3ca14bc48bf0e7f95d2e0043bf0f248646
nilesh5995 <nilesh_s@techjoomla.com>
2020-07-22 17:25:55 +0530

    solve the og-image issue

--- src/components/common/site-metadata.js
+++ src/components/common/site-metadata.js
@@ -3,81 +3,74 @@ import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql, withPrefix } from 'gatsby';
 import favicon from '../../../static/img/favicon/favicon-32x32.png'
 import fontURL from "./fonts/overpass-extralight.woff"
-import { globalHistory } from '@reach/router'
 
-
-const SEO = props  => {
-      const pathname = globalHistory.location.pathname;
-      const { site } = useStaticQuery(detailsQuery);
-      const title = props.title || site.siteMetadata.title;
-      const metakeywords = props.metakeywords
-      const metadescription = props.metadescription
-      var ogImage = props.ogimage
-      if(ogImage) {
-         ogImage = ogImage.childImageSharp.fluid.src;
-        } else {
-          ogImage = site.siteMetadata.defaultImage;
-        }
-      var url = pathname && pathname != '/' ? process.env.HOST_URL+pathname : process.env.HOST_URL;
-      return (
-        <Helmet
-          htmlAttributes={{
-            lang: 'en',
-          }}
-          title={title}
-          titleTemplate={`%s - ${site.siteMetadata.title}`}
-          link={[
-            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
-          ]}
-        >
-        <html lang="en" />
-        <link rel="preload"
-            as="font"
-            href={fontURL}
-            type="font/woff2"
-            crossOrigin="anonymous" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/favicon/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon/favicon-16x16.png`}
-          sizes="16x16"
-        />
-        {metadescription ? <meta name="description" content={metadescription} /> : ''}
-        {metakeywords ? <meta name="keywords" content={metakeywords} /> : ''}
-        <meta name="theme-color" content="#089add" />
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:site" content="tekdi.net"/>
-        {metadescription ? <meta name="twitter:description" content={metadescription}/> : ''}
-        <meta property="og:type" content="website" />
-        {title ? <meta property="og:title" content={title} /> : ''}
-        {metadescription ? <meta property="og:description" content={metadescription} /> :''}
-        {url ? <meta property="og:url" content={url} /> : ''}
-        <meta property="og:image" content={process.env.HOST_URL+ogImage} />
-        {ogImage ? <meta name="twitter:image" content={process.env.HOST_URL+ogImage} /> : ''}
-      </Helmet>
-      );
-    }
+const SEO = (props)  => {
+    const { site } = useStaticQuery(detailsQuery);
+    const title = props.title || site.siteMetadata.title;
+    const metakeywords = props.metakeywords
+    const metadescription = props.metadescription
+    var ogImage = props.ogimage
+    if(ogImage) {
+        ogImage = ogImage.childImageSharp.fluid.src;
+      } else {
+        ogImage = site.siteMetadata.defaultImage;
+      }
+    var url = process.env.HOST_URL;
+    return (
+      <Helmet
+        htmlAttributes={{
+          lang: 'en',
+        }}
+        title={title}
+        titleTemplate={`%s - ${site.siteMetadata.title}`}
+        link={[
+          { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
+        ]}
+      >
+      <html lang="en" />
+      <link rel="preload"
+          as="font"
+          href={fontURL}
+          type="font/woff2"
+          crossOrigin="anonymous" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={`${withPrefix('/')}img/favicon/apple-touch-icon.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href={`${withPrefix('/')}img/favicon/favicon-32x32.png`}
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href={`${withPrefix('/')}img/favicon/favicon-16x16.png`}
+        sizes="16x16"
+      />
+      {metadescription ? <meta name="description" content={metadescription} /> : ''}
+      {metakeywords ? <meta name="keywords" content={metakeywords} /> : ''}
+      <meta name="theme-color" content="#089add" />
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta name="twitter:site" content="tekdi.net"/>
+      {metadescription ? <meta name="twitter:description" content={metadescription}/> : ''}
+      <meta property="og:type" content="website" />
+      {title ? <meta property="og:title" content={title} /> : ''}
+      {metadescription ? <meta property="og:description" content={metadescription} /> :''}
+      {url ? <meta property="og:url" content={url} /> : ''}
+      <meta property="og:image" content={process.env.HOST_URL+ogImage} />
+      {ogImage ? <meta name="twitter:image" content={process.env.HOST_URL+ogImage} /> : ''}
+    </Helmet>
+    );
+  }
 
 SEO.defaultProps = {
   lang: 'en',
   meta: [],
   keywords: [],
 };
-
-// SEO.propTypes = {
-//   pathname: PropTypes.string,
-// }
 
 export default SEO;
 
