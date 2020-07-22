34199c49511875e1a5e93efa614f62a383bcecd9
nilesh5995 <nilesh_s@techjoomla.com>
2020-07-22 15:54:05 +0530

    solve the issue of og:image is not working

--- src/components/common/site-metadata.js
+++ src/components/common/site-metadata.js
@@ -1,37 +1,31 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { StaticQuery, graphql, withPrefix } from 'gatsby';
+import { StaticQuery, useStaticQuery, graphql, withPrefix } from 'gatsby';
 import favicon from '../../../static/img/favicon/favicon-32x32.png'
 import fontURL from "./fonts/overpass-extralight.woff"
+import { globalHistory } from '@reach/router'
+import PropTypes from "prop-types"
 
-const SEO = props => (
-  <StaticQuery
-    query={detailsQuery}
-    render={(data) => {
-      const title = props.title || data.site.siteMetadata.title;
+const SEO = props  => {
+      const pathname = globalHistory.location.pathname;
+      const { site } = useStaticQuery(detailsQuery);
+      const title = props.title || site.siteMetadata.title;
       const metakeywords = props.metakeywords
       const metadescription = props.metadescription
       var ogImage = props.ogimage
-      console.log(ogImage);
       if(ogImage) {
          ogImage = ogImage.childImageSharp.fluid.src;
         } else {
-          ogImage = data.site.siteMetadata.defaultImage;
+          ogImage = site.siteMetadata.defaultImage;
         }
-        
-      var url =  typeof window !== 'undefined' ? window.location.href : '';
-      if(url) {
-        var arr = url.split("/");
-        var ogImageUrl = arr[0] + "//" + arr[2]
-      }
-      //var ogImageUrl =  typeof window !== 'undefined' ? window.location.hostname : '';
+      var url = pathname && pathname != '/' ? process.env.HOST_URL+pathname : process.env.HOST_URL;
       return (
         <Helmet
           htmlAttributes={{
             lang: 'en',
           }}
           title={title}
-          titleTemplate={`%s - ${data.site.siteMetadata.title}`}
+          titleTemplate={`%s - ${site.siteMetadata.title}`}
           link={[
             { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
           ]}
@@ -59,7 +53,7 @@ const SEO = props => (
           href={`${withPrefix('/')}img/favicon/favicon-16x16.png`}
           sizes="16x16"
         />
-       {metadescription ? <meta name="description" content={metadescription} /> : ''}
+        {metadescription ? <meta name="description" content={metadescription} /> : ''}
         {metakeywords ? <meta name="keywords" content={metakeywords} /> : ''}
         <meta name="theme-color" content="#089add" />
         <meta name="twitter:card" content="summary_large_image"/>
@@ -69,19 +63,21 @@ const SEO = props => (
         {title ? <meta property="og:title" content={title} /> : ''}
         {metadescription ? <meta property="og:description" content={metadescription} /> :''}
         {url ? <meta property="og:url" content={url} /> : ''}
-        <meta property="og:image" content={ogImageUrl+ogImage} />
-        {ogImageUrl && ogImage ? <meta name="twitter:image" content={ogImageUrl+ogImage} /> : ''}
+        <meta property="og:image" content={process.env.HOST_URL+ogImage} />
+        {ogImage ? <meta name="twitter:image" content={process.env.HOST_URL+ogImage} /> : ''}
       </Helmet>
       );
-    }}
-  />
-);
+    }
 
 SEO.defaultProps = {
   lang: 'en',
   meta: [],
   keywords: [],
 };
+
+// SEO.propTypes = {
+//   pathname: PropTypes.string,
+// }
 
 export default SEO;
 
