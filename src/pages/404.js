a0b305452f0da62bc20a3328a8dc8a8c69263194
nilesh5995 <nilesh_s@techjoomla.com>
2020-08-03 16:27:20 +0530

    Change the structure of the 404 page not found page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout/baselayout';
+import Banner from '../components/common/banner/banner';
 
 const NotFoundPage = () => (
   <Layout>
+     <Banner
+            bannerTitle = "NOT FOUND"
+          />
     <div>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
